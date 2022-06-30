15c72e359b28c8dc348abeab8b6466cf1f174ee1
taikyunn <taichi_141@me.com>
2022-06-30 17:21:48 +0900

    ログアウト処理
    blade中心であったためサクッと実行

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -1,21 +1,29 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
-
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
-
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
   <ul class="navbar-nav ml-auto">
-
+    <!-- ユーザーがまだログインしていない時の処理 -->
+    @guest
     <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a> {{--この行を変更--}}
     </li>
-
+    @endguest
+    <!-- ここまで -->
+    <!-- ユーザーがまだログインしていない時の処理 -->
+    @guest
     <li class="nav-item">
       <a class="nav-link" href="">ログイン</a>
     </li>
-
+    @endguest
+    <!-- ここまで -->
+    <!-- ユーザーがログイン済みの場合の処理 -->
+    @auth
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
-
+    @endauth
+    <!-- ここまで -->
+    <!-- ユーザーがログイン済みの場合の処理 -->
+    @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
@@ -33,10 +41,11 @@
         </button>
       </div>
     </li>
-    <form id="logout-button" method="POST" action="">
+    <form id="logout-button" method="POST" action="{{ route('logout') }}">
+      @csrf
     </form>
     <!-- Dropdown -->
-
+    @endauth
+    <!-- ここまで -->
   </ul>
-
 </nav>
