cf8bdb415da56b385bc8d14c7a0bc74436c666cb
taikyunn <taichi_141@me.com>
2022-06-30 17:10:42 +0900

    登録画面の作成
    bladeに関する部分だったのでサクッと

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\ as ServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
