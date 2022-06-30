b1f2b417fcc39a6efb5bdb23969e0eef36493503
taikyunn <taichi_141@me.com>
2022-06-30 11:30:15 +0900

    DBデータ取得の形に修正
    モデルを使って、コントローラーからデータを取得する形に修正。
    
    1.Articleモデルをコントローラーで使用できるようにimportする
    2.allメソッドを使用してデータを取得する
    
    Laravelにはコレクションという便利機能が備わっている。
    これは、配列をより楽に操作できるものである。
    データの並び替えだったり、データの抽出や剪定をしたりと様々なことが行える。

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -3,62 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Articleモデルを使用するためにimport 以後Articleという名前でArticleモデルを使用できる
+use App\Article;
 
 class ArticleController extends Controller
 {
-    /* コントローラーの雛形作成コマンド
-    php artisan make:controller コントローラー名
-    */
+    /**
+     * コントローラーの雛形作成コマンド
+     * php artisan make:controller コントローラー名
+     *
+     */
 
-    // インデックスアクションを
+    // インデックスアクションを定義する
     public function index() {
-        $articles =  [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
-        /* viewメソッド
-        第一引数:ビューファイル (今回はresources/views/articles/index)
-        第二引数:ビューファイルに渡す変数の名称と、その変数の値を連想配列形式で指定する
+        /**
+         * allメソッド
+         * モデルテーブルの全レコードを結果として返すメソッド
+         *
+         * sortByDesc
+         * 降順にソートして取得する
+         * 
+         * コレクション
+         * PHPの配列を拡張したものでLaravelに用意されたクラス
+         * コレクションは配列と同じように扱うことができるが、配列にはない様々なメソッドを使用することができる
+         */
+        $articles = Article::all()->sortByDesc('created_at');
 
-        withメソッド
-        withメソッドの引数にビューファイルに渡す変数の名称とその連想配列を指定することで返すことができる
-
-        return view('articles.index')->with(['articles' => $articles]);
-
-        compact関数
-        compact関数を使うと変数を連想配列形式で記述する必要がなくなる。コードがすっきりする。
-        viewの変数とcontrollerで定義した変数の名前が一致する際、compact関数を使って返すことができる。
-
-        return view('articles.index', compact('articles'));
-        */
+        /**
+         * viewメソッド
+         * 第一引数:ビューファイル (今回はresources/views/articles/index)
+         * 第二引数:ビューファイルに渡す変数の名称と、その変数の値を連想配列形式で指定する
+         *
+         * withメソッド
+         * withメソッドの引数にビューファイルに渡す変数の名称とその連想配列を指定することで返すことができる
+         * return view('articles.index')->with(['articles' => $articles]);
+         *
+         * compact関数
+         * compact関数を使うと変数を連想配列形式で記述する必要がなくなる。コードがすっきりする。
+         * viewの変数とcontrollerで定義した変数の名前が一致する際、compact関数を使って返すことができる。
+         * return view('articles.index', compact('articles'));
+         */
         return view('articles.index', ['articles' => $articles]);
 
     }
